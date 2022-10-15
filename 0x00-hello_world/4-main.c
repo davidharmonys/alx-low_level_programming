f072061319a8cc77bb2b0dff69ee52a3fbfc0c5d
David Harmony <davidharmony50@gmail.com>
2022-10-15 08:43:52 -0700

    Hello Puts

--- 0x00-hello_world/4-main.c
+++ 0x00-hello_world/4-main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
-int main(void)
 
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+	
 {
-	puts("/"Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
